5694a8555053424eb2ec3784c4be354474b42ee1
Jacob <jacobchanrahan@gmail.com>
2021-12-07 16:51:04 +0000

    Fixing RSpec

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "user#index"
 
-  # get 'user/index' # commeting out but we might not need this
+  get 'user/index'
   get "/user", to: "user#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
