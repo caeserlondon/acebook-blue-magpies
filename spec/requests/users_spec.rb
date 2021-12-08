71f9f92b5a75f09a8a3b739fbd01afdc599faaed
Blessing <blessing@Blessings-MacBook-Air.local>
2021-12-08 15:02:19 +0000

    solved rspec error

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/user/index"
+      get "/users/index"
       expect(response).to have_http_status(:success)
     end
   end
