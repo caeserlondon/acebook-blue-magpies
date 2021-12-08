fec3eb902f0751d4b950f393b2304d1c789bb602
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-12-08 16:58:04 +0000

    comments submitting and showing on post page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,11 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    p @comment
     redirect_to post_path(@post)
   end
 
   private
   def comment_params
-    params.require(:comment_body).permit(:comment_body)
+    params.require(:comment).permit(:comment_body)
   end
 end
