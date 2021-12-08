e9992a50d5493db26eee788edfaa001a01c6ef4f
Matt Zimmer <zimmermja@gmail.com>
2021-12-08 11:54:29 +0000

    added new and create methods to UserController; renamed methods to 'new'

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,8 +2,18 @@ class UserController < ApplicationController
   def index
   end
 
-  def sign_up
+  def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
 end
