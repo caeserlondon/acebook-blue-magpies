fa8f43f9416a9279dbd266269a7e690db1a9c793
Jacob <jacobchanrahan@gmail.com>
2021-12-08 11:15:46 +0000

    fixing Sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "/sign_up", to: "user#sign_up"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :posts
+  resources :users, :posts
 end
