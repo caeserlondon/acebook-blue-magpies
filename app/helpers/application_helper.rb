038ae8477651c8499568cbfe8ce4e90aa5e9484c
Jacob <jacobchanrahan@gmail.com>
2021-12-13 15:51:53 +0000

    Fixed session ending when logging out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,10 @@ def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if !!session[:user_id]
 end
 
-def log_out
-    session.delete[:user_id]
-    @current_user = nil
-  end
+# def log_out
+#     session.delete[:user_id]
+#     @current_user = nil
+# end
 end
 
 
