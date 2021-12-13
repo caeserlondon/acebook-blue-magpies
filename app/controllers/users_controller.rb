31dac3d02747f11cf11fb0ccae801ca761f29b4d
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-12-13 16:34:00 +0000

    Users have own profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
 
   def show
     shout "show page loaded"
-    @user = User.find(session[:user_id])
-    
+    @user = User.find(params[:id])
+    #@user = User.find(session[:user_id])
   end
 
   def create
