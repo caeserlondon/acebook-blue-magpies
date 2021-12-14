1744ac17dd50f4aee9137209e81ede488c0ec37e
Simon Jacobs <simon_jacobs_1990@hotmail.co.uk>
2021-12-14 11:17:47 +0000

    updated some rspec tests for navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     
     def destroy
         # User.find(session[:user_id]).destroy      
-        session[:user_id] = nil         
+        session[:user_id] = nil
         redirect_to root_path
       end
 end
