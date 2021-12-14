9e0c873a748fb9ff7e6b6f64ad756b3949cd1e1e
88Andybird <anbird88@gmail.com>
2021-12-14 14:49:03 +0000

    Added destroy post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,14 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    # render :posts_url
+    redirect_to posts_path
+  end
     
  
 
